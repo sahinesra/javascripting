325e845d278227f406016c857ce4e58b03a0dcc1
Esra Sahin <sahinesra@gmail.com>
2021-11-12 00:57:37 -0500

    Initial commit with my js work so far

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (ding,dang,dong) {
+  return (dang*dong)+ding
+
+}
+console.log(math(53,61,67))
+
---
+++ string-length.js
@@ -0,0 +1,5 @@
+
+var example = 'example string';
+var z = example.length;
+console.log(z);
+
